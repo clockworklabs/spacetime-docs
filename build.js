24c56edc0a8a49ba3c3025ce5ae1b3e5e1c8bad7
Puru <awesomepuruvj@gmail.com>
2024-05-01 20:22:24 +0530

    Push

---
+++ build.js
@@ -0,0 +1,28 @@
+// @ts-check
+import { build } from 'tsup';
+import { createContext, Script } from 'node:vm';
+import { readFile, writeFile, rm } from 'node:fs/promises';
+import { inspect } from 'node:util';
+
+await build({ entry: { out: 'nav.ts' }, clean: true, format: 'esm' });
+
+// Read the file
+const nav = await readFile('dist/out.js', 'utf8');
+
+// Remove this line
+// export {
+// nav
+// };
+const final = nav.replace(/export {[^}]*};/, '') + '\nnav;';
+
+// Execute the code
+const context = createContext();
+const script = new Script(final);
+const out = script.runInContext(context);
+
+await writeFile(
+	'docs/nav.js',
+	'export default ' + inspect(out, { depth: null, compact: false, breakLength: 120 })
+);
+
+await rm('dist/out.js', { recursive: true });
